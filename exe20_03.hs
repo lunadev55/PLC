c94cfc9c27bce86d1735e00f2f5e172335f21552
André Luna <alm3@cin.ufpe.br>
2018-03-21 23:40:38 -0300

    functions over lists 20/03

--- exe20_03.hs
+++ exe20_03.hs
@@ -1,11 +1,11 @@
---funcoes sobre listas
+-- FUNCTIONS OVER LISTS
 -- PART 1 
 
 -- doubles each elem in a list
 double :: [Int] -> [Int]
 double xs = [x^2 | x <- xs]
 
--- checks if a give element is part of the list
+-- checks if a given element is part of the list
 member :: [Int] -> Int -> Bool
 member [] _ = False
 member (l:ls) x
@@ -29,10 +29,44 @@ type BancoDados = [(Pessoa, Livro)]
 baseExemplo :: BancoDados
 baseExemplo = [("Joao","Software Abstractions"), ("Andre","Programming in Haskell"), ("Fernando", "Introduction to Programming with Python"), ("Fernando","Programming in Haskell")]
 
+-- NOT USING LIST COMPREHENSION
+
 livros :: BancoDados -> Pessoa -> [Livro]
 livros [] _ = []
 livros ((a,b):ls) x 
     | a == x = [b] ++ (livros ls x)
     | otherwise = livros ls x
+
+emprestimos :: BancoDados -> Livro -> [Pessoa]
+emprestimos [] _ = []
+emprestimos ((a,b):ls) x
+	| b == x = [a] ++ (emprestimos ls x)
+	| otherwise = emprestimos ls x
+
+emprestado :: BancoDados -> Livro -> Bool
+emprestado [] _ = False
+emprestado ((a,b):ls) x
+	| b == x = True
+	| otherwise = emprestado ls x
+
+qtdEmprestimos :: BancoDados -> Pessoa -> Int
+qtdEmprestimos [] _ = 0
+qtdEmprestimos ((a,b):ls) x
+	| a == x = 1 + qtdEmprestimos ls x
+	| otherwise = qtdEmprestimos ls x
+
+-- having trouble
+emprestar :: BancoDados -> Pessoa -> Livro -> BancoDados
+emprestar [] _ = []
+emprestar ((a,b):ls) = ls ++ [(a,b)]
+
+
+
+
+
+
+
+
+
 
 
